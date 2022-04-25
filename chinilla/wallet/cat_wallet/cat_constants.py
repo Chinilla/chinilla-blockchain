ac8b1750f34bf9be2aaff6ed638856a19eca171c
Edward Teach <edward@edwardteach.co>
2022-04-25 14:56:19 -0700

    changes for 1.0.3

--- chinilla/wallet/cat_wallet/cat_constants.py
+++ chinilla/wallet/cat_wallet/cat_constants.py
@@ -1,9 +1,16 @@
-CHINILLA_HOLIDAY_TOKEN = {
-    "asset_id": "509deafe3cd8bbfbb9ccce1d930e3d7b57b40c964fa33379b18d628175eb7a8f",
-    "name": "Chinilla Holiday 2021 Token",
-    "symbol": "CH21",
+CHINILLA_FOUNDER_TOKEN = {
+    "asset_id": "ded689d844ab7dc5cf50f3fb041d8db633941ec75e83bc111011b19c25d761df",
+    "name": "Founder Token",
+    "symbol": "CFT",
+}
+
+CHINILLA_EARLY_FARMER = {
+    "asset_id": "30178eaedeae2bb83a476246754cfc82ae381dd135ba02b5d3435a2852c9154d",
+    "name": "Early Farmer",
+    "symbol": "CEF",
 }
 
 DEFAULT_CATS = {
-    CHINILLA_HOLIDAY_TOKEN["asset_id"]: CHINILLA_HOLIDAY_TOKEN,
+    CHINILLA_FOUNDER_TOKEN["asset_id"]: CHINILLA_FOUNDER_TOKEN,
+    CHINILLA_EARLY_FARMER["asset_id"]: CHINILLA_EARLY_FARMER,
 }
